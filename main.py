139b5af665c25446ca6d71fd80224c173660b29d
NotKilleration <77356224+NotKilleration@users.noreply.github.com>
2023-01-06 00:46:27 +0530

    rock paper scissors!

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -27,3 +29,24 @@ scissors = '''
 
 #Write your code below this line 👇
 
+game_images = [rock, paper, scissors]
+
+userc = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+print(game_images[userc])
+
+compc = random.randint(0, 2)
+print("Computer chose:")
+print(game_images[compc])
+
+if userc >= 3 or userc < 0: 
+  print("You typed an invalid number, you lose!") 
+elif userc == 0 and compc == 2:
+  print("You win!")
+elif compc == 0 and userc == 2:
+  print("You lose")
+elif compc > userc:
+  print("You lose")
+elif userc > compc:
+  print("You win!")
+elif compc == userc:
+  print("It's a draw")
